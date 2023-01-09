0e3a09d35c3a180798b0a87a88c505e9b38b8bd5
Javier Mtz <javiermtz@MacBook-Air-de-Javier.local>
2023-01-09 16:42:35 -0600

    First Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMM_Example"
+include(":androidApp")
+include(":shared")
